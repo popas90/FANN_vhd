3fdacb612570bce21e05c873dbccfb57f70ab4db
Bogdan Popa <bogdititupopa@gmail.com>
2014-03-07 22:54:10 +0200

    Added CoreMaster and PkgUtilities.

---
+++ Package/PkgUtilities.vhd
@@ -0,0 +1,9 @@
+package PkgUtilities is
+  
+  type CoreOperation_t is (NoOperation, ForwardPass, BackwardPass);
+  
+end package PkgUtilities;
+
+package body PkgUtilities is
+  
+end package body PkgUtilities;
