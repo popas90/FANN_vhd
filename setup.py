4c832f5cbc5b1282036ee3d46eb5c2e14cd6ff65
popas90 <bogdititupopa@gmail.com>
2016-03-26 14:23:55 +0200

    add license info

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     name="nose-switch",
     version=VERSION,
     author="Bogdan Popa",
-    description="Add special switches in code, \
-        based on options set when running tests.",
+    license="GNU GPL",
+    description="Add special switches in code, based on options set when running tests.",
     url="https://github.com/popas90/nose-switch",
     py_modules=['nose_switch'],
     entry_points={'nose.plugins': ['nose_switch = nose_switch:NoseSwitch']},
