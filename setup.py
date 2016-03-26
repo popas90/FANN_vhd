1923ba4e442d821c5a005e87b1c0cd8a092d5ac6
popas90 <bogdititupopa@gmail.com>
2016-03-26 16:11:53 +0200

    edit setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@
 from distutils.core import setup
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 setup(
     name="nose-switch",
     packages=['nose-switch'],
     version=VERSION,
     author="Bogdan Popa",
+    author_email="bogdititupopa@gmail.com",
     license="GNU GPL",
     description="Add special switches in code, based on options set when running tests.",
     url="https://github.com/popas90/nose-switch",
     download_url="https://github.com/popas90/nose-switch/tarball/0.1.0",
-    py_modules=['nose_switch'],
-    entry_points={'nose.plugins': ['nose_switch = nose_switch:NoseSwitch']},
-    install_requires=['nose'],
-    test_suite='tests',
     classifiers=[],
 )
